579a1b7f45f280d90f613b353f9e98c9400fef98
Julius Suominen <julius.suominen@gmail.com>
2017-09-03 20:13:10 +0200

    video 16

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router';
+import Photo from './Photo';
+import Comments from './Comments';
+// import comments
 
 const Single = React.createClass({
   render() {
+
+    const { postId } = this.props.params;
+    // get index of post
+    const i = this.props.posts.findIndex((post) => post.code === postId);
+    // get post
+    const post = this.props.posts[i];
+    const postComments = this.props.comments[postId] || [];
+
     return (
       <div className="single-photo">
-        Single photo
+        <Photo i={i} post={post} {...this.props} />
+        <Comments postComments={postComments} {...this.props} />
       </div>
     )
   }
--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,4 +1,35 @@
+function postComments(state = [], action) {
+  switch(action.type) {
+
+    case 'ADD_COMMENT':
+      // return the new state with the new comment
+      return [...state, {
+        user: action.author,
+        text: action.comment
+      }];
+    
+    case 'REMOVE_COMMENT':
+      // need to return state without deleted comment
+      return [
+        ...state.slice(0, action.i),
+        ...state.slice(action.i + 1)
+      ]
+
+    default:
+      return state;
+  }
+  return state;
+}
+
 function comments(state = [], action) {
+  if(typeof action.postId !== 'undefined') {
+    return {
+      // take current state
+      ...state,
+      // overwrite post with a new one
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
   return state;
 }
 
